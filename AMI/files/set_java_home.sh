a5fe0e3a4229400695999cb363e45e155532dd51
ctienshi <ctienshi@gmail.com>
2018-04-24 11:46:20 +0530

    Adding CF template for APIM Pattern1

--- AMI/files/set_java_home.sh
+++
@@ -1,2 +0,0 @@
-JAVA_HOME=/opt/java
-PATH=/opt/java/bin:$PATH
--- sync-lock/start-database.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=lock_db -d mysql:5.5
